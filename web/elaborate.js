e4839d1faa51ab830f7f1e6ddc7dfbdbb8733eef
Calum Mackervoy <c.mackervoy@gmail.com>
2018-11-11 22:03:34 +0000

    web version

---
+++ web/elaborate.js
@@ -0,0 +1,17 @@
+
+/*
+*   Copy the JavaScript below for use in your own project
+*   The .hover() function call will need to be duplicated for each new overlay
+*/
+
+function overlayUniqueIdOn(uniqueId) {
+    document.getElementById(uniqueId).style.display = "block";
+}
+
+function overlayUniqueIdOff(uniqueId) {
+    document.getElementById(uniqueId).style.display = "none";
+} 
+
+/*
+*   End of marked JavaScript
+*/
